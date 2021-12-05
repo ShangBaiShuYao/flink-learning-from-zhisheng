52703745bd0b3bdc3dba862afd0ea1bdbb5903d1
shangbaishuyao <shangbaishuyao@163.com>
2021-12-05 10:40:36 +0800

    自定flume拦截

--- flume/src/main/java/com/shangbaishuyao/flume/TimeStampInterceptor2.java
+++ flume/src/main/java/com/shangbaishuyao/flume/TimeStampInterceptor2.java
@@ -5,25 +5,19 @@ import com.alibaba.fastjson.JSONObject;
 import org.apache.flume.Context;
 import org.apache.flume.Event;
 import org.apache.flume.interceptor.Interceptor;
-
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Map;
 
 /**
- * <p>消费者 flume</p>
- *  file文件 --> flume --> kafka -- flume   此为 kafka --> flume 部分 <br/>
- * 解决的思路：拦截json日志，通过fastjson框架解析json，获取实际时间ts。
- * 将获取的ts时间写入拦截器header头，header的key必须是timestamp，因为Flume框架会根据这个key的值识别为时间，写入到HDFS。
- * @author shangbaishuyao
- * @create 2021-11-14 下午3:21
+ *<p> flume 拦截器<p/>
+ * @author 徐霸天
+ * @create 2021-11-28 下午19:38
  */
 
 public class TimeStampInterceptor2 implements Interceptor {
     @Override
-    public void initialize() {
-
-    }
+    public void initialize() {}
 
     @Override
     public Event intercept(Event event) {
