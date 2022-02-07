ec36e88ddcacfb9d2146f53439e29136050d8a10
shangbaishuyao <shangbaishuyao@163.com>
2022-02-07 16:30:02 +0800

    Flink处理案例: 泛型擦除现象

--- flink-1.14.3-Demo/src/main/java/www/xubatian/cn/FlinkDemo01/Flink_WordCount_Bounded.java
+++ flink-1.14.3-Demo/src/main/java/www/xubatian/cn/FlinkDemo01/Flink_WordCount_Bounded.java
@@ -1,19 +1,18 @@
-package com.shangbaishuyao.demo.FlinkDemo01;
+package www.xubatian.cn.FlinkDemo01;
 
-import com.shangbaishuyao.demo.Function.MyFlatMapFunc2;
-import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.java.functions.KeySelector;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.KeyedStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import www.xubatian.cn.Function.MyFlatMapFunc3;
 
-/*
- * @author: 徐霸天
- * @des:
- * @date: 上午10:56 2021/12/5
- **/
+/**
+ * @author shangbaishuyao
+ * @create 2022-02-07 下午2:15
+ */
+
 public class Flink_WordCount_Bounded {
     public static void main(String[] args) throws Exception{
         //1.获取执行环境
@@ -22,17 +21,19 @@ public class Flink_WordCount_Bounded {
         //2.读取文件创建流
         DataStreamSource<String> input = env.readTextFile("/Users/shangbaishuyao/warehouse/IDEA_WorkSpace/Flink_WorkSpace/flink-learning-from-zhisheng/flink-1.14.3-Demo/input/word.txt");
         //3.压平并将单词转换为元组
-        SingleOutputStreamOperator<Tuple2<String, Integer>> wordToOneDS = input.flatMap(new MyFlatMapFunc2());
+        SingleOutputStreamOperator<Tuple2<String, Integer>> wordToOneDS = input.flatMap(new MyFlatMapFunc3());
         //4.分组  TODO 批处理里面有groupBy , 但是流里面没有 ,只能用keyBy
         KeyedStream<Tuple2<String, Integer>, String> keyedStream = wordToOneDS.keyBy(
-                (KeySelector<Tuple2<String, Integer>, String>) value -> {
-                    return value.f0; //scala的第一个元素时f_1,但是java是f0
-                })/*.returns(Types.TUPLE(Types.STRING,Types.INT))*/; //Lambda表达需要指明返回值类型
+                new KeySelector<Tuple2<String, Integer>, String>() {
+                    @Override
+                    public String getKey(Tuple2<String, Integer> value) throws Exception {
+                        return value.f0; //scala的第一个元素时f_1,但是java是f0
+                    }
+                });
         //5.按照Key做聚合操作
         SingleOutputStreamOperator<Tuple2<String, Integer>> result = keyedStream.sum(1);
         //6.打印结果
         result.print();
         //7.启动任务
-        env.execute("Flink_WordCount_Bounded");
-    }
+        env.execute("Flink_WordCount_Bounded");    }
 }
